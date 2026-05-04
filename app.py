ef13e42759df139fe08cbaa2cb7f95900f968f07
NIRBAN RAY <nirbanray@NIRBANs-MacBook-Air.local>
2026-05-04 14:10:22 +0530

    Stable version before optimization

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ from analytics import log_usage
 import pandas as pd
 from analytics import conn
 
+@st.cache_data(show_spinner=False)
+def get_study_data():
+    return all_units
+
+@st.cache_data(show_spinner=False)
+def get_model_data():
+    return model_answers_data
+
+@st.cache_data(show_spinner=False)
+def get_revision_data():
+    return quick_revision_data
+
+@st.cache_data(show_spinner=False)
+def get_exam_data():
+    return exam_zone_data
+
 # UNIT I CONTENT
 topic1 = """
 ## ভূ-কৌশলগত অৱস্থান আৰু সমাজ-সংস্কৃতিক বৈচিত্ৰ্য
@@ -1401,12 +1417,11 @@ menu = st.session_state.get("menu")
 
 if menu == "পাঠ্য সামগ্ৰী":
 
-    render_units(all_units)
+    render_units(get_study_data())
+    st.cache_data.clear()
+    st.rerun()
 
 
-# -----------------------------------
-# IMPORTANT QUESTIONS
-# -----------------------------------
 if menu == "Search Topic":
 
     st.title("Smart Topic Finder")
@@ -1457,28 +1472,33 @@ if menu == "Search Topic":
     if normalized_keyword and not results_found:
         st.warning("No matching topic found.")
 
+        st.cache_data.clear()
+        st.rerun()
+
 elif menu == "Exam Zone":
-    render_units(exam_zone_data)
-    
+    render_units(get_exam_data())
+    st.cache_data.clear()
+    st.rerun()
 # -----------------------------------
 # QUICK REVISION
 # -----------------------------------
 elif menu == "Model Answers": 
-  render_units(model_answers_data)
+  render_units(get_model_data())
+  
 
 elif menu == "দ্ৰুত পুনৰালোচনা":
-
+  
     st.header("⚡ Quick Revision")
 
     selected_unit = st.selectbox(
         "ইউনিট নিৰ্বাচন কৰক",
-        list(quick_revision_data.keys()),
+        list(get_revision_data().keys()),
         key="revision_unit"
     )
 
     st.markdown("---")
 
-    for item in quick_revision_data[selected_unit]:
+    for item in get_revision_data()[selected_unit]:
         st.markdown(f"- {item}")
 
 elif menu == "Visual Learning Zone":
